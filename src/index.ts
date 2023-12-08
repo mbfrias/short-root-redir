eca885bfc3e253789e87c38fa03db3664d943dab
martin <martin@tub3.uk>
2023-12-08 22:36:52 +0000

    midflare

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,27 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+addEventListener('fetch', (event) => {
+    event.respondWith(handleRequest(event.request));
+  });
+  
+  async function handleRequest(request: any) {
+    // Make a request to your desired URL
+    const url = 'https://landingpages.mrtin.co/shortener?domain=mvrt.in';
+    const response = await fetch(url);
+  
+    // Get the response body
+    let data = await response.text();
 
-import handleProxy from './proxy';
-import handleRedirect from './redirect';
-import apiRouter from './router';
-
-// Export a default object containing event handlers
-export default {
-	// The fetch handler is invoked when this worker receives a HTTP(S) request
-	// and should return a Response (optionally wrapped in a Promise)
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		// You'll find it helpful to parse the request.url string into a URL object. Learn more at https://developer.mozilla.org/en-US/docs/Web/API/URL
-		const url = new URL(request.url);
-
-		// You can get pretty far with simple logic like if/switch-statements
-		switch (url.pathname) {
-			case '/redirect':
-				return handleRedirect.fetch(request, env, ctx);
-
-			case '/proxy':
-				return handleProxy.fetch(request, env, ctx);
-		}
-
-		if (url.pathname.startsWith('/api/')) {
-			// You can also use more robust routing
-			return apiRouter.handle(request);
-		}
-
-		return new Response(
-			`Try making requests to:
-      <ul>
-      <li><code><a href="/redirect?redirectUrl=https://example.com/">/redirect?redirectUrl=https://example.com/</a></code>,</li>
-      <li><code><a href="/proxy?modify&proxyUrl=https://example.com/">/proxy?modify&proxyUrl=https://example.com/</a></code>, or</li>
-      <li><code><a href="/api/todos">/api/todos</a></code></li>`,
-			{ headers: { 'Content-Type': 'text/html' } }
-		);
-	},
-};
+    // change all instances of "./" to "https://landingpages.mrtin.co/"
+    data = data.replace(/"\.\//g, '"https://landingpages.mrtin.co/');
+    // change all instances of "/_next/" to "https://landingpages.mrtin.co/_next/"
+    data = data.replace(/"\/_next\//g, '"https://landingpages.mrtin.co/_next/');
+  
+    // Customize the response if needed
+    const modifiedResponse = new Response(data, {
+      headers: {
+        'Content-Type': 'text/html',
+      },
+    });
+  
+    return modifiedResponse;
+  }
+  
